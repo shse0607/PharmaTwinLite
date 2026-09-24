a795936165beed5ac4b9133ae4a71e00a76443e1
shse0607 <60111922+shse0607@users.noreply.github.com>
2026-09-24 13:20:41 -0600

    Add Tank class definition in Tank.h

---
+++ src/Tank.h
@@ -0,0 +1,21 @@
+C++
+#ifndef TANK_H
+#define TANK_H
+ 
+class Tank
+{
+private:
+double capacity;
+double currentLevel;
+ 
+public:
+Tank(double cap);
+ 
+void addLiquid(double amount);
+ 
+void removeLiquid(double amount);
+ 
+double getLevel() const;
+};
+ 
+#endif
