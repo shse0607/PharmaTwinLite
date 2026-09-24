342e00501b46b0337f91f9cf9af5bec89470a30f
shse0607 <60111922+shse0607@users.noreply.github.com>
2026-09-24 13:21:25 -0600

    Implement Tank class with basic liquid management

---
+++ src/Tank.cpp
@@ -0,0 +1,22 @@
+#include "Tank.h"
+ 
+Tank::Tank(double cap)
+{
+capacity = cap;
+currentLevel = 0;
+}
+ 
+void Tank::addLiquid(double amount)
+{
+currentLevel += amount;
+}
+ 
+void Tank::removeLiquid(double amount)
+{
+currentLevel -= amount;
+}
+ 
+double Tank::getLevel() const
+{
+return currentLevel;
+}
