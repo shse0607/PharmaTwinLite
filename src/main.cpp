dbbe4b1bbaecbb3243485b786735cf5e122fc58e
shse0607 <60111922+shse0607@users.noreply.github.com>
2026-09-25 18:31:09 +0000

    Cleaning up code

--- src/main.cpp
+++ src/main.cpp
@@ -45,35 +45,3 @@ std::cout << "Overflow Alarm is active!" << std::endl;
 return 0;
 }
 
-
-
-/*
-#include <iostream>
-#include "Tank.h"
-#include "FillValve.h"
-#include "DrainValve.h"
-
-
-int main()
-{
-  Tank tank(100); // initialize tank with capacity of 100
-
-  FillValve valve(5); // initialize fill valve with flow rate of 5
-  valve.open(); // open the fill valve
-
-  if(valve.isValveOpen())
-  {
-    tank.addLiquid(valve.getFlowRate());//  add liquid to the tank
-  } 
-
-  //tank.addLiquid(10000);
-  //tank.removeLiquid(50);
-  
-  std::cout << "Tank Level: "
-            << tank.getLevel()
-            << std::endl;
-
-return 0;
-}
-*/
-
